8d9d5493173927a7bca81d3e04d86574eacba9d2
Niklas Molnes Hole <nikasmh@hotmail.com>
2023-12-01 00:39:09 +0100

    Style the chat

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Textarea } from "@/components/ui/textarea";
+import { AlertCircle } from "lucide-react";
 import { useState, KeyboardEvent } from "react";
 
 type Message = {
@@ -40,31 +47,64 @@ export default function Chat() {
   };
 
   const handleKeyDown = (event: KeyboardEvent<HTMLTextAreaElement>) => {
-    if (event.key === "Enter" && event.shiftKey === false) {
+    if (!loading && event.key === "Enter" && event.shiftKey === false) {
+      event.preventDefault();
       sendMessage();
     }
   };
 
   return (
-    <div>
-      {messages.map((m, i) => (
-        <div key={i}>
-          {m.role}: {m.content}
+    <div className="flex flex-col gap-8">
+      <h1 className="text-4xl">Chat</h1>
+      <div className="flex flex-col gap-4">
+        {messages.map((message, i) => (
+          <Card key={i}>
+            <CardContent className="pt-6 relative">
+              {message.role === "assistant" ? <Badge className="absolute top-[-10px] left-4">GPT</Badge> : null}
+              {message.content}
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+
+      {loading && (
+        <div className="flex items-center space-x-4">
+          <Skeleton className="h-12 w-12 rounded-full" />
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-[250px]" />
+            <Skeleton className="h-4 w-[200px]" />
+          </div>
         </div>
-      ))}
+      )}
 
-      {loading && <div>Loading...</div>}
-      {error && !loading && <div className="text-red-500">{error}</div>}
+      {error && !loading && (
+        <Alert color="red">
+          <AlertCircle className="h-4 w-4" />
+          <AlertTitle>Error</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      )}
 
-      <textarea
+      <Textarea
+        className="p"
         value={input}
         placeholder="Say something..."
         onChange={(e) => setInput(e.target.value)}
         onKeyDown={handleKeyDown}
       />
-      <button onClick={sendMessage} disabled={!input}>
-        Send
-      </button>
+      <div className="flex flex-row gap-4">
+        <Button onClick={sendMessage} disabled={!input}>
+          Send
+        </Button>
+        <Button
+          onClick={() => {
+            setMessages([]);
+          }}
+          variant="destructive"
+        >
+          Reset
+        </Button>
+      </div>
     </div>
   );
 }
