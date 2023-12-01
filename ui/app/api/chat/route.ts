3c34aff2eabc1d2fcc913a7e8b4427d0db6463a1
Niklas Molnes Hole <nikasmh@hotmail.com>
2023-12-01 19:32:01 +0100

    Remove console logs

--- ui/app/api/chat/route.ts
+++ ui/app/api/chat/route.ts
@@ -5,15 +5,6 @@ const apiBase = process.env["AZURE_OPENAI_API_BASE_URL"];
 const deploymentModel = process.env["AZURE_OPENAI_API_MODEL"] || "gpt-4";
 const apiVersion = process.env["AZURE_OPENAI_API_VERSION"] || "2023-10-01-preview";
 
-console.log("API KEY", apiKey);
-console.log("API BASE", apiBase);
-console.log("DEPLOYMENT MODEL", deploymentModel);
-console.log("API VERSION", apiVersion);
-
-if (!apiKey) {
-  throw new Error("The AZURE_OPENAI_API_KEY environment variable is missing or empty.");
-}
-
 const openai = new OpenAI({
   apiKey,
   baseURL: `${apiBase}/openai/deployments/${deploymentModel}`,
