98d7d650e52300dc22ee1962910ce81538c9fa3b
Niklas Molnes Hole <nikasmh@hotmail.com>
2023-12-02 13:14:18 +0100

    Update function name in image-edit page

--- ui/app/image-edit/page.tsx
+++ ui/app/image-edit/page.tsx
@@ -7,7 +7,7 @@ import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 
-export default function Chat() {
+export default function ImageEdit() {
   const [input, setInput] = useState("A cute baby sea otter wearing a beret");
   const [image, setImage] = useState("");
   const [loading, setLoading] = useState(false);
